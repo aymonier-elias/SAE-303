01a5dbc82e9867ba61bca7a06075536030eafba1
Elias Aymonier <elias.aymonier.pro@gmail.com>
2026-01-18 20:12:54 +0100

    Carte interactive qui renvoie sur la page du departement clicker

---
+++ script/carte.js
@@ -0,0 +1,18 @@
+// Récupération des mes elements
+const carte = document.querySelector(".carte");
+
+
+// Le but ici est envoyer le user sur la page du départementt sur lequel il a cliquer \\
+
+// Event qui renvoie l'id de la parttie du svg voulue
+carte.addEventListener("click", e => {
+    // Récupération des parties du svg qui m'interesse
+    const path = e.target.closest("path");
+    const g = e.target.closest("g");
+
+    if (path && path.id) {
+        window.location.href = `departement.html?dep=${path.id}`;
+    } else if (g && g.id) {
+        window.location.href = `departement.html?dep=${g.id}`;
+    }
+});
